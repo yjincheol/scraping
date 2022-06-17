2083f426cdf82027b70d8275a8b13da0df2223ec
jincheol <yjc3232@gmail.com>
2022-06-17 11:06:39 +0900

    webtoon test

--- bs4_test.py
+++ bs4_test.py
@@ -14,4 +14,20 @@ soup = BeautifulSoup(res.text,"lxml")
 # print(soup.title.get_text()) #네이버 웹툰 > 요일별  웹툰 > 전체웹툰
 # print(soup.a.attrs) #{'href': '#menu', 'onclick': "document.getElementById('menu').tabIndex=-1;document.getElementById('menu').focus();return false;"}
 # print(soup.a['href']) #menu
-print(soup.find('a', attrs={'class': 'Nbtn_upload'})) #<a class="Nbtn_upload" href="/mypage/myActivity" onclick="nclk_v2(event,'olk.upload');">웹툰 올리기</a>
+# print(soup.find('a', attrs={'class': 'Nbtn_upload'})) #<a class="Nbtn_upload" href="/mypage/myActivity" onclick="nclk_v2(event,'olk.upload');">웹툰 올리기</a>
+# print(soup.find('li',attrs={'class': 'rank01'}))
+rank1 = soup.find('li',attrs={'class': 'rank01'})
+# print(rank1.a.get_text())
+# rank2 = rank1.next_sibling.next_sibling
+# rank3 = rank2.next_sibling.next_sibling
+# print(rank3.a.get_text())
+# rank2 = rank3.previous_sibling.previous_sibling
+# print(rank2.a.get_text())
+# print(rank1.parent)
+# rank2 = rank1.find_next_sibling('li')
+# print(rank2.a.get_text())
+# rank3 = rank2.find_next_sibling('li')
+# print(rank3.a.get_text())
+# rank2 = rank3.find_previous_sibling('li')
+# print(rank2.a.get_text())
+print(rank1.find_next_siblings('li'))
