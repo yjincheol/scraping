a234926622d38c8c7fecc559d190743b31d0614e
jincheol <yjc3232@gmail.com>
2022-06-21 14:23:14 +0900

    coupang_notebook(info : name,price,rate,page,link), Apple(x)

--- coupang_notebook.py
+++ coupang_notebook.py
@@ -1,45 +1,50 @@
-
 import requests
 import re
 from bs4 import BeautifulSoup
 
-url = 'https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=1=6&backgroundColor='
+
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36'}
-res = requests.get(url,headers=headers )
-res.raise_for_status()
+for i in range(1,6): #1-5페이지 
+    print("페이지 : ", i)
+    url = 'https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page={}&rocketAll=false&searchIndexingToken=1=6&backgroundColor='.format(i)
+    res = requests.get(url,headers=headers )
+    res.raise_for_status()
 
-soup = BeautifulSoup(res.text,"lxml")
+    soup = BeautifulSoup(res.text,"lxml")
 
-items = soup.find_all("li",attrs={"class":re.compile("^search-product")})
-# print(items[0].find("div",attrs={"class":"name"}).get_text())
-for item in items:
-    #광고 제품 제외
-    ad_badge = item.find("span",attrs={"class":ad-badge-text})
-    if ad_badge:
-        print("광고상품 제외합니다")
-        continue
-    name = item.find("div",attrs={"class":"name"}).get_text() #제품명
+    items = soup.find_all("li",attrs={"class":re.compile("^search-product")})
+    # print(items[0].find("div",attrs={"class":"name"}).get_text())
+    for item in items:
+        #광고 제품 제외
+        ad_badge = item.find("span",attrs={"class":ad-badge-text})
+        if ad_badge:
+            print("광고상품 제외합니다")
+            continue
+        name = item.find("div",attrs={"class":"name"}).get_text() #제품명
 
-    # 애플 상품 제외
-    if "Apple" in name:
-        print("Apple 상품 제외합니다")
-    price = item.find("strong",attrs={"class":"price-value"}).get_text() #가격
+        # 애플 상품 제외
+        if "Apple" in name:
+            print("Apple 상품 제외합니다 ")
+        price = item.find("strong",attrs={"class":"price-value"}).get_text() #가격
 
-    # 리뷰 100개 이상, 평점 4.5 이상 되는 것만 조회
-    rate = item.find("em",attrs={"class":"rating"}) #평점
-    if rate:
-        rate = rate.get_text()
-    else:
-        print("평점 없는 상품 제외합니다")
-        continue
+        # 리뷰 100개 이상, 평점 4.5 이상 되는 것만 조회
+        rate = item.find("em",attrs={"class":"rating"}) #평점
+        if rate:
+            rate = rate.get_text()
+        else:
+            print("평점 없는 상품 제외합니다")
+            continue
 
-    rate_cnt = item.find("span",attrs={"class":"rating-total-count"}) #평점 수
-    if rate_cnt:
-        rate_cnt = rate_cnt.get_text() # ex. (26)
-        rate_cnt = rate_cnt[1:-1] # ex. 26
-    else:
-        print("평점 수 없는 상품 제외합니다")
-        continue 
-    
-    if float(rate) >=4.5 and int(rate_cnt) >=100:  
-        print(name,price,rate,rate_cnt)
+        rate_cnt = item.find("span",attrs={"class":"rating-total-count"}) #평점 수
+        if rate_cnt:
+            rate_cnt = rate_cnt.get_text()[1:-1] # ex. (26) - >26
+        else:
+            print("평점 수 없는 상품 제외합니다")
+            continue 
+        
+        link = item.find("a",attrs={"class":"search-product-link"})
+        if float(rate) >=4.5 and int(rate_cnt) >=100:  
+            print(f'제품명 : {name}')
+            print(f'가격 : {price}')
+            print(f'평점 : {rate}점 ({rate_cnt}개)')
+            print("바로가기 {}".format("https://www.coupang.com"+link))
